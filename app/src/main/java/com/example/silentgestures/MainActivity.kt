a63981d7d551cd0d1304263580460af0f751968a
aiweiii <aiwei.ong.2020@scis.smu.edu.sg>
2023-03-10 21:40:35 +0800

    add navigation

--- app/src/main/java/com/example/silentgestures/MainActivity.kt
+++ app/src/main/java/com/example/silentgestures/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.silentgestures
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Button
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startButton = findViewById<Button>(R.id.startButton)
+
+        startButton.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
