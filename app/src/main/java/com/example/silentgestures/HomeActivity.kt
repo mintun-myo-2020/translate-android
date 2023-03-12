58976213e492a73a677b5cd646e0be73e878935b
aiweiii <aiwei.ong.2020@scis.smu.edu.sg>
2023-03-12 14:06:34 +0800

    add navigation

--- app/src/main/java/com/example/silentgestures/HomeActivity.kt
+++ app/src/main/java/com/example/silentgestures/HomeActivity.kt
@@ -1,12 +1,26 @@
 package com.example.silentgestures
 
-import androidx.appcompat.app.AppCompatActivity
+//import android.R
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragment)
+
+        bottomNavigationView.setupWithNavController(navController)
+
     }
 }
