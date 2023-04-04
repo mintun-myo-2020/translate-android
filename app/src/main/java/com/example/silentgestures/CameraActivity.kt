3a602e0db65d522edd41bdb142c5c8d12d7682bf
mintun-myo-2020 <mintun.myo.2020@scis.smu.edu.sg>
2023-04-04 16:08:50 +0800

    add object deetection helper

--- app/src/main/java/com/example/silentgestures/CameraActivity.kt
+++ app/src/main/java/com/example/silentgestures/CameraActivity.kt
@@ -2,13 +2,52 @@ package com.example.silentgestures
 
 //import android.R
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.Camera
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.core.VideoCapture
+import androidx.camera.lifecycle.ProcessCameraProvider
+import com.example.silentgestures.databinding.ActivityCameraBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.io.FileOutputStream
+import java.util.concurrent.ExecutorService
 
 
 class CameraActivity : AppCompatActivity() {
+
+    private val TAG = "ObjectDetection"
+
+    private var _activityCameraBinding: ActivityCameraBinding? = null
+
+    private val activityCameraBinding
+        get() = _activityCameraBinding!!
+
+    private lateinit var objectDetectorHelper: ObjectDetectorHelper
+    private lateinit var bitmapBuffer: Bitmap
+    private var preview: Preview? = null
+    private var imageAnalyzer: ImageAnalysis? = null
+    private var camera: Camera? = null
+    private var cameraProvider: ProcessCameraProvider? = null
+
+    /** Blocking camera operations are performed using this executor */
+    private lateinit var cameraExecutor: ExecutorService
+    private var videoCapture: VideoCapture? = null
+    private lateinit var buttonRecord: Button
+    private val REQUEST_CODE_PERMISSIONS = 1001
+
+
+    private var showWords:String? = ""
+    private var prevWord:String? = ""
+    private var words = ArrayList<String>()
+//    private var words = arrayListOf("")
+
+    private val txtFileName = ""
+    private lateinit var outputStream: FileOutputStream
+    private var appendWords:String? = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
