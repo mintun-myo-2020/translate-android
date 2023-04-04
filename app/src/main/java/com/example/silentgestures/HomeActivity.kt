628f38bb4b1d020f1c2c81a32f33ea8989a630dd
mintun-myo-2020 <mintun.myo.2020@scis.smu.edu.sg>
2023-04-04 15:08:46 +0800

    camera layout stuff done

--- app/src/main/java/com/example/silentgestures/HomeActivity.kt
+++ app/src/main/java/com/example/silentgestures/HomeActivity.kt
@@ -17,10 +17,6 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        val navController = findNavController(R.id.fragment)
-
-        bottomNavigationView.setupWithNavController(navController)
 
     }
 }
--- app/src/main/java/com/example/silentgestures/MainActivity.kt
+++ app/src/main/java/com/example/silentgestures/MainActivity.kt
@@ -1,17 +1,48 @@
 package com.example.silentgestures
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.Button
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 
 class MainActivity : AppCompatActivity() {
+    private val CAMERA_PERMISSION_CODE = 1
+    private val AUDIO_PERMISSION_CODE = 2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Check if camera permission is granted
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Request camera permission if not granted
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                CAMERA_PERMISSION_CODE
+            )
+        }
+
+        // Check if audio permission is granted
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Request audio permission if not granted
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.RECORD_AUDIO),
+                AUDIO_PERMISSION_CODE
+            )
+        }
 
         val startButton = findViewById<Button>(R.id.startButton)
 
@@ -20,4 +51,35 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    // Handle permission request results
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            CAMERA_PERMISSION_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission granted for camera
+                    Toast.makeText(this, "Camera permission granted", Toast.LENGTH_SHORT).show()
+                } else {
+                    // Permission denied for camera
+                    Toast.makeText(this, "Camera permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+            AUDIO_PERMISSION_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission granted for audio
+                    Toast.makeText(this, "Audio permission granted", Toast.LENGTH_SHORT).show()
+                } else {
+                    // Permission denied for audio
+                    Toast.makeText(this, "Audio permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
 }
